698598f3bbd8ac9a3fcba6bd9d542e7b42da1f34
Tony <tjiang11@jhu.edu>
2017-03-24 18:26:09 -0500

    FAQ Update. Release 1.5

--- app/src/main/java/com/toniebalonie/tjiang11/tcrunch/StudentInfoDialog.java
+++ app/src/main/java/com/toniebalonie/tjiang11/tcrunch/StudentInfoDialog.java
@@ -22,6 +22,19 @@ public class StudentInfoDialog extends DialogFragment {
             "improve teaching and education in the classroom.</p>" +
 
             "<p>Tcrunch was developed from funding from the Johns Hopkins Center for Educational Resources.</p>" +
+
+            "<h5>Why does the app need my name?</h5>" +
+            "<p>This will be how the teacher will recognize if you submitted the exit ticket or not. Your response will be tied to your name, unless explicitly marked by the teacher to be a ticket with only anonymous responses. If this is the case, you will see an indication on the submit response page that your response will be anonymous.</p>" +
+
+            "<h5>What is the class code and how do I get it?</h5>" +
+            "<p>The class code is a unique code defined for every class created by a teacher in Tcrunch. The teacher will have to provide this information to students.</p>" +
+
+            "<h5>How do I know that I submitted the ticket correctly?</h5>" +
+            "<p>When you log into your app you, you will see exit tickets either under sections labeled \"Not Answered\" or \"Answered.\" If you see your exit ticket in the \"Answered\" section, then you know that you have successfully submitted your exit ticket.</p>" +
+
+            "<h5>How do I see the exit tickets for just one class?</h5>" +
+            "<p>Click the menu button on the upper left hand side of the app. From there you can select the class of interest. Now only the exit tickets for this class will be available to see.</p>" +
+
             "</big>";
 
     @Override
--- app/src/main/java/com/toniebalonie/tjiang11/tcrunch/TeacherInfoDialog.java
+++ app/src/main/java/com/toniebalonie/tjiang11/tcrunch/TeacherInfoDialog.java
@@ -30,8 +30,33 @@ public class TeacherInfoDialog extends DialogFragment {
             "<p>Tcrunch is a mobile-platform method for exit ticket administration and collection. Our app decreases the " +
             "time it takes to make and launch such a ticket. Our mission is to decrease the time for data collection and " +
             "analysis for teachers who can use this teaching technique to improve teaching and education in the classroom.</p>" +
-            "<p>Tcrunch was developed from funding from the Johns Hopkins Center for Educational Resources.</p>" + "</big>";
+            "<p>Tcrunch was developed from funding from the Johns Hopkins Center for Educational Resources.</p>" +
 
+            "<h5>How do I create a class?</h5>" +
+            "<p>At the home screen for teachers click \"Add Class\" found in the upper right hand side of the app. Give the new class a unique name and a class code to share with your students so that they can access the tickets for the class. Tap the menu button on the upper left side of the app to access and see the new class you have created.</p>" +
+
+            "<h5>How do I create an exit ticket?</h5>" +
+            "<p>After creating a class, select the class that you would like to create an exit ticket in. Click the red \"+\" button in the bottom right hand side of the app. Set a date and time that you would like to launch the exit ticket. If you attempt to select a time in the past, then the time will be set to now and the ticket will launch immediately. Type the question in the field below and click create. If you want to use a multiple choice question, then check the Multiple Choice checkbox and type in your choices.</p>" +
+
+            "<h5>What is a class code?</h5>" +
+            "<p>When creating a new class, you will be asked to enter a class code. You will share this class code with your students to allow them to access your created tickets.</p>" +
+
+            "<h5>How do I see my students' responses?</h5>" +
+            "<p>Your students responses will come in real time. Click on the exit ticket of interest. You will be able to see the responses below in the order that they were submitted. If the ticket was multiple choice, then you will see a bar graph of student responses. Additionally you can download a csv file with the data to also view the results of the exit ticket.</p>" +
+
+            "<h5>How do I download my results for exit tickets?</h5>" +
+            "<p>First click on the exit ticket of interest. Then click on the options menu in the upper right hand side of the app. Click \"Email Ticket Data\" and select how you would like to send the data. It will be sent as a csv file, which you could open on spreadsheet programs such as Microsoft Excel or Google Sheets.</p>" +
+
+            "<h5>How do I delete an exit ticket?</h5>" +
+            "<p>First click on the exit ticket of interest. Then click on the options menu in the upper right hand side of the app. Click \"Delete Ticket.\" You will be asked if you are sure you would like to delete the ticket because this action cannot be undone. Click yes if so.</p>" +
+
+            "<h5>How can I have the students answer anonymously?</h5>" +
+            "<p>When creating a new ticket, there will be a checkbox labeled \"Anonymous Responses\". If checked, then all student responses will be anonymous. Students will see an indication before submitting that their response will be anonymous. You will still be able to view which students submitted responses after exporting your data via email, but responses will not be tied to their authors.</p>" +
+
+            "<h5>How do I delete a class?</h5>" +
+            "<p>Select the class you would like to delete from the menu button on the upper left side of the app. You should now see the class in the main screen of the app with any of the exit tickets you have created. Now click the options button in the upper right hand side of the app. Select \"delete class.\" You will be prompted if you want to delete this class. Press yes if this is the right class to delete. This action cannot be undone so make sure this is the class you want to delete.</p>" +
+
+            "</big>";
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         TextView infoView = new TextView(getActivity());
